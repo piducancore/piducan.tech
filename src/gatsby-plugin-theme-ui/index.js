c1c8e51369649b74596733a1f228aa7540076ccb
piducancore:/# <hola@piducan.dev>
2021-10-16 10:43:48 -0300

    assign to a variable before exporting

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,4 +1,4 @@
-export default {
+const theme = {
   config: {
     useCustomProperties: true,
     initialColorMode: "light",
@@ -55,3 +55,5 @@ export default {
     },
   },
 };
+
+export default theme;
