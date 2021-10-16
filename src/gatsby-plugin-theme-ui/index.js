4907937cd58e9c3c36b1030ea223cd729156ead0
piducancore:/# <hola@piducan.dev>
2021-10-16 20:31:06 -0300

    button animation

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,8 +2,22 @@ import base from "@theme-ui/preset-base";
 import { merge } from "theme-ui";
 
 const theme = merge(base, {
-  fonts: {
-    body: "monospace",
+  buttons: {
+    primary: {
+      cursor: "pointer",
+      transition: "all 0.2s",
+      bg: "primary",
+      color: "background",
+      fontSize: 3,
+      "&:hover": {
+        bg: "background",
+        color: "primary",
+        fontSize: 5,
+      },
+      "&:active": {
+        fontSize: 6,
+      },
+    },
   },
 });
 
