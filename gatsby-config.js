b0e13b7b8960d9ca095b57c8e9822af7bb50705f
piducancore:/# <hola@piducan.dev>
2021-10-21 17:28:13 -0300

    add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  siteMetadata: {
+    title: `piducAn.tech ~/`,
+    description: `bashing keyboards since debian potato.`,
+    author: `@piducancore`,
+    url: `https://piducan.tech`,
+  },
   plugins: ["gatsby-plugin-theme-ui"],
 };
